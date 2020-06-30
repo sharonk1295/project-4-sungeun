bb2121d7aab68299ca86d7d193111fadc2ca4e11
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-06-29 21:44:20 -0400

    added css modules for header and layout components

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
-        <header>
+        <header className={headerStyles.header}>
             <div>
-                <h1>Sungeun Sul, RD CDN</h1>
+                <h1><Link className={headerStyles.title} to="/">Sungeun Sul, RD CDN</Link></h1>
                 <p>Luxuriating wellness through proper eating.</p>
             </div>
             <nav>
                 <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/contact">Contact Me</Link></li>
+                    <li><Link className={headerStyles.link} activeClassName={headerStyles.activeLink} to='/'>Home</Link></li>
+                    <li><Link className={headerStyles.link} activeClassName={headerStyles.activeLink} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.link} activeClassName={headerStyles.activeLink} to="/blog">Blog</Link></li>
+                    <li><Link className={headerStyles.link} activeClassName={headerStyles.activeLink} to="/contact">Contact Me</Link></li>
                 </ul>
             </nav>
         </header>
