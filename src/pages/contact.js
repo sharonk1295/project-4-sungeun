50956d798fa3f7929337500dad3127a3f37f6068
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-05 22:43:17 -0400

    added react icons for contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Head from '../components/head'
+import { FaLinkedin } from 'react-icons/fa'
+import {IconContext } from 'react-icons'
 import contactStyles from './contact.module.css'
+
 
 const Contact = () => {
     return (
         <Layout>
             <Head title="Contact Me" />
             <h1 className={contactStyles.header}>Connect With Me:</h1>
-            <a href="https://www.linkedin.com/in/sung-eun-sul-rd-cdn-685272163/" target="_blank" rel="noreferrer">LinkedIn</a>
+            <div className={contactStyles.contacts}>
+                <IconContext.Provider value={{color: `green`, size: `3rem`}}>
+                    <a href="https://www.linkedin.com/in/sung-eun-sul-rd-cdn-685272163/" target="_blank" rel="noreferrer"><FaLinkedin aria-label="Mute volume"/></a>
+                </IconContext.Provider>
+            </div>
+            
         </Layout>
     )
 }
